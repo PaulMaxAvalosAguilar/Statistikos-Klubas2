c8b76a83b324e02a521d49c14c2143039c420ed9
Paul Max Avalos Aguilar <pauldromeasaurio@hotmail.com>
2017-09-14 16:17:24 -0500

    Added license

--- Statistikos/src/main/java/Main/Controllers/MainController.java
+++ Statistikos/src/main/java/Main/Controllers/MainController.java
@@ -1,5 +1,5 @@
-/*
- * Copyright (C) 2017 
+/* 
+ * Copyright (C) 2017 Paul Max Avalos Aguilar at S.D.T. pauldromeasaurio@hotmail.com
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
